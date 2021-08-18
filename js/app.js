bf0d7b078f6a44e634f04926b29c24b1a4c2b81d
shahriarAb <sm.abeed007@gmail.com>
2021-08-18 19:42:54 +0600

    code updated

--- js/app.js
+++ js/app.js
@@ -1,43 +1,51 @@
-function sixDigitPin(){
-    const pinGenerate = Math.round(Math.random()*1000000);
+function sixDigitPin() {
+    const pinGenerate = Math.round(Math.random() * 1000000);
     const pinLength = pinGenerate + ''; //make number to a string
-    if(pinLength.length == 6){
+    if (pinLength.length == 6) {
         return pinGenerate;
     }
-    else{
+    else {
         return sixDigitPin();
     }
 }
-function generatePin(){
+function generatePin() {
     const pin = sixDigitPin();
     document.getElementById('display-pin').value = pin;
 }
-document.getElementById('key-pad').addEventListener('click',function(e){
+document.getElementById('key-pad').addEventListener('click', function (e) {
     const screen = document.getElementById('screen');
     const buttonText = e.target.innerText;
-    if(isNaN(buttonText)){
-        if(buttonText == 'C'){
+    if (isNaN(buttonText)) {
+        if (buttonText == 'C') {
             screen.value = '';
         }
-        else if(buttonText == '<'){
+        else if (buttonText == '<') {
             screen.value = screen.value.slice(0, -1);
         }
     }
-    else{
+    else {
         screen.value += buttonText;
     }
 });
-function submitBtn(){
+function submitBtn() {
+    document.getElementById('submit-btn').disabled = false;
     const generatedPin = document.getElementById('display-pin').value;
     const typedPin = document.getElementById('screen').value;
     const wrongPin = document.getElementById('wrong-pin');
     const correctPin = document.getElementById('correct-pin');
-    if(generatedPin == typedPin){
+    if (generatedPin == typedPin) {
         correctPin.style.display = 'block';
         wrongPin.style.display = 'none';
     }
-    else{
+    else {
         correctPin.style.display = 'none';
         wrongPin.style.display = 'block';
+        const tryLeft = document.getElementById('try-left');
+        if (tryLeft.innerText > 0) {
+            tryLeft.innerText = parseInt(tryLeft.innerText) - 1;
+        }
+        else{
+            document.getElementById('submit-btn').disabled = true;
+        }
     }
 }
