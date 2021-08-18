686549481dfe2e0730c46a346554f5f5214597c3
shahriarAb <sm.abeed007@gmail.com>
2021-08-18 19:01:21 +0600

    pin matcher done

---
+++ js/app.js
@@ -0,0 +1,43 @@
+function sixDigitPin(){
+    const pinGenerate = Math.round(Math.random()*1000000);
+    const pinLength = pinGenerate + ''; //make number to a string
+    if(pinLength.length == 6){
+        return pinGenerate;
+    }
+    else{
+        return sixDigitPin();
+    }
+}
+function generatePin(){
+    const pin = sixDigitPin();
+    document.getElementById('display-pin').value = pin;
+}
+document.getElementById('key-pad').addEventListener('click',function(e){
+    const screen = document.getElementById('screen');
+    const buttonText = e.target.innerText;
+    if(isNaN(buttonText)){
+        if(buttonText == 'C'){
+            screen.value = '';
+        }
+        else if(buttonText == '<'){
+            screen.value = screen.value.slice(0, -1);
+        }
+    }
+    else{
+        screen.value += buttonText;
+    }
+});
+function submitBtn(){
+    const generatedPin = document.getElementById('display-pin').value;
+    const typedPin = document.getElementById('screen').value;
+    const wrongPin = document.getElementById('wrong-pin');
+    const correctPin = document.getElementById('correct-pin');
+    if(generatedPin == typedPin){
+        correctPin.style.display = 'block';
+        wrongPin.style.display = 'none';
+    }
+    else{
+        correctPin.style.display = 'none';
+        wrongPin.style.display = 'block';
+    }
+}
